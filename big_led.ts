221f38723f9ca042cedce308fd3ed91885fcaa28
sclim <kiate.lim@gmail.com>
2023-02-22 10:03:47 +0800

    Move RekabitPortYellowPin to big_led.ts. Deleted main.ts and main.blocks

--- big_led.ts
+++ big_led.ts
@@ -6,6 +6,19 @@
  * Email:   support@cytron.io
  *******************************************************************************/
 
+enum RekabitPortYellowPin {
+    //% block="P0"
+    P0 = AnalogPin.P0,
+    //% block="P1"
+    P1 = AnalogPin.P1,
+    //% block="P2"
+    P2 = AnalogPin.P2,
+    //% block="P13"
+    P13 = AnalogPin.P13,
+    //% block="P15"
+    P15 = AnalogPin.P15,
+}
+
 
 // IO state.
 enum RekabitDigitalIoState {
